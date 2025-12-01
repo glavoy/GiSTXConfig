9a48fc86896c65cb1ec1ea9440d9e6f9ca28546c
Geoff Lavoy <glavoy@pm.me>
2025-12-01 14:32:39 +0300

    Added config file for generation of survey_manifest.gistx config file

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -152,17 +152,23 @@ namespace generatexml
                     connection.Open();
 
                     string createTableQuery = @"
-                        CREATE TABLE IF NOT EXISTS crfs (
-	                        tablename	TEXT,
-	                        primarykey	TEXT,
-	                        displayname	TEXT,
-	                        isbase	INTEGER DEFAULT 0,
-	                        linkingfield	TEXT,
-	                        parenttable	TEXT,
-	                        incrementfield	TEXT,
-	                        requireslink	INTEGER DEFAULT 0,
-	                        idconfig	TEXT
-                        )";
+                                CREATE TABLE crfs (
+                                  display_order INTEGER DEFAULT 0, 
+                                  tablename TEXT,
+                                  primarykey TEXT,
+                                  displayname TEXT,
+                                  isbase INTEGER DEFAULT 0,
+                                  linkingfield TEXT,
+                                  parenttable TEXT,
+                                  incrementfield TEXT,
+                                  requireslink INTEGER DEFAULT 0,
+                                  idconfig TEXT,
+                                  repeat_count_field TEXT, 
+                                  repeat_count_source TEXT, 
+                                  auto_start_repeat INTEGER, 
+                                  repeat_enforce_count INTEGER,
+                                  display_fields TEXT
+                                )";
 
                     using (var command = new SQLiteCommand(createTableQuery, connection))
                     {
@@ -186,28 +192,100 @@ namespace generatexml
                     connection.Open();
 
                     string insertQuery =
-                        "INSERT INTO crfs (tablename, primarykey, displayname, isbase, linkingfield, parenttable, incrementfield, requireslink, idconfig) " +
-                        "VALUES (@tablename, @primarykey, @displayname, @isbase, @linkingfield, @parenttable, @incrementfield, @requireslink, @idconfig)";
+                        "INSERT INTO crfs (" +
+                        "display_order, tablename, primarykey, displayname, isbase, linkingfield, parenttable, " +
+                        "incrementfield, requireslink, idconfig, repeat_count_field, " +
+                        "auto_start_repeat, repeat_enforce_count, display_fields) " +
+                        "VALUES (@display_order, @tablename, @primarykey, @displayname, @isbase, @linkingfield, @parenttable, " +
+                        "@incrementfield, @requireslink, @idconfig, @repeat_count_field, " +
+                        "@auto_start_repeat, @repeat_enforce_count, @display_fields)";
 
+                    Excel.Range usedRange = crf_ws.UsedRange;
+                    int lastRow = usedRange.Rows.Count;
+
+                    using (var transaction = connection.BeginTransaction())
                     using (var insertCommand = new SQLiteCommand(insertQuery, connection))
                     {
-                        Excel.Range usedRange = crf_ws.UsedRange;
-                        for (int row = 2; row <= usedRange.Rows.Count; row++)
+                        // Ensure this command participates in the transaction
+                        insertCommand.Transaction = transaction;
+
+                        object GetCellValue(int r, int c)
+                        {
+                            try
+                            {
+                                var val = ((Excel.Range)usedRange.Cells[r, c]).Value2;
+                                if (val is string s && string.IsNullOrWhiteSpace(s)) return null;
+                                return val;
+                            }
+                            catch
+                            {
+                                return null;
+                            }
+                        }
+
+                        int ToInt(object o, int defaultValue = 0)
+                        {
+                            if (o == null) return defaultValue;
+                            if (o is double d) return Convert.ToInt32(d);
+                            if (o is int i) return i;
+                            if (int.TryParse(o.ToString(), out int parsed)) return parsed;
+                            return defaultValue;
+                        }
+
+                        for (int row = 2; row <= lastRow; row++) // assume header is the first row of usedRange
                         {
                             insertCommand.Parameters.Clear();
 
-                            insertCommand.Parameters.AddWithValue("@tablename", ((Excel.Range)usedRange.Cells[row, 1]).Value2);
-                            insertCommand.Parameters.AddWithValue("@primarykey", ((Excel.Range)usedRange.Cells[row, 2]).Value2);
-                            insertCommand.Parameters.AddWithValue("@displayname", ((Excel.Range)usedRange.Cells[row, 3]).Value2);
-                            insertCommand.Parameters.AddWithValue("@isbase", ((Excel.Range)usedRange.Cells[row, 4]).Value2 ?? 0);
-                            insertCommand.Parameters.AddWithValue("@linkingfield", ((Excel.Range)usedRange.Cells[row, 5]).Value2);
-                            insertCommand.Parameters.AddWithValue("@parenttable", ((Excel.Range)usedRange.Cells[row, 6]).Value2);
-                            insertCommand.Parameters.AddWithValue("@incrementfield", ((Excel.Range)usedRange.Cells[row, 7]).Value2);
-                            insertCommand.Parameters.AddWithValue("@requireslink", ((Excel.Range)usedRange.Cells[row, 8]).Value2 ?? 0);
-                            insertCommand.Parameters.AddWithValue("@idconfig", ((Excel.Range)usedRange.Cells[row, 9]).Value2);
+                            // Excel columns (per your order):
+                            // 1 display_order
+                            // 2 tablename
+                            // 3 primarykey
+                            // 4 displayname
+                            // 5 isbase
+                            // 6 linkingfield
+                            // 7 parenttable
+                            // 8 incrementfield
+                            // 9 requireslink
+                            // 10 idconfig
+                            // 11 repeat_count_field
+                            // 12 auto_start_repeat
+                            // 13 repeat_enforce_count
+                            // 14 display_fields
+
+                            var rawDisplayOrder = GetCellValue(row, 1);
+                            var rawTablename = GetCellValue(row, 2);
+                            var rawPrimaryKey = GetCellValue(row, 3);
+                            var rawDisplayName = GetCellValue(row, 4);
+                            var rawIsBase = GetCellValue(row, 5);
+                            var rawLinkingField = GetCellValue(row, 6);
+                            var rawParentTable = GetCellValue(row, 7);
+                            var rawIncrementField = GetCellValue(row, 8);
+                            var rawRequiresLink = GetCellValue(row, 9);
+                            var rawIdConfig = GetCellValue(row, 10);
+                            var rawRepeatCountField = GetCellValue(row, 11);
+                            var rawAutoStartRepeat = GetCellValue(row, 13);
+                            var rawRepeatEnforceCount = GetCellValue(row, 14);
+                            var rawDisplayFields = GetCellValue(row, 15);
+
+                            insertCommand.Parameters.AddWithValue("@display_order", ToInt(rawDisplayOrder, 0));
+                            insertCommand.Parameters.AddWithValue("@tablename", (object)rawTablename ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@primarykey", (object)rawPrimaryKey ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@displayname", (object)rawDisplayName ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@isbase", ToInt(rawIsBase, 0));
+                            insertCommand.Parameters.AddWithValue("@linkingfield", (object)rawLinkingField ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@parenttable", (object)rawParentTable ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@incrementfield", (object)rawIncrementField ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@requireslink", ToInt(rawRequiresLink, 0));
+                            insertCommand.Parameters.AddWithValue("@idconfig", (object)rawIdConfig ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@repeat_count_field", (object)rawRepeatCountField ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@auto_start_repeat", ToInt(rawAutoStartRepeat, 0));
+                            insertCommand.Parameters.AddWithValue("@repeat_enforce_count", ToInt(rawRepeatEnforceCount, 0));
+                            insertCommand.Parameters.AddWithValue("@display_fields", (object)rawDisplayFields ?? DBNull.Value);
 
                             insertCommand.ExecuteNonQuery();
                         }
+
+                        transaction.Commit();
                     }
                 }
             }
@@ -217,6 +295,7 @@ namespace generatexml
                 logstring.Add("ERROR: Could not add data to crfs table. " + ex.Message);
             }
         }
+
 
         public void CopyMasterTables(string sourceDatabasePath, string db_path, string[] sourceTableNames)
         {
